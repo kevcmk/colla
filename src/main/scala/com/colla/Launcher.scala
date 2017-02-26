c9befa63906fffd74767590a77e687004c693b09
Kevin Katz <kevcmk@gmail.com>
2017-02-25 19:15:19 -0800

    Threading fixed.

--- src/main/scala/com/colla/Launcher.scala
+++ src/main/scala/com/colla/Launcher.scala
@@ -49,52 +49,50 @@ object Launcher {
             //              .take(1386)
 
             // Select a random sample
-            val rng = Random.setSeed(42)
+
+            Random.setSeed(42)
+            val targetCount = 1386
+
             val metaSelection = metadata
               .filter(_.dateTime.isDefined) // Filter all that had invalid datetimes
               .filter(_.crop.width > 1280) // Filter all that are > 1280
               .map(x => (Random.nextFloat(), x))
               .sortBy(_._1)
               .map(_._2)
-              .take(1386)
-
-            // Minimum Pixel Edge Width
-            val minCrop = metaSelection
-              .groupBy(_.crop)
-              .map(x => x._1 -> x._2.length)
-              .toSeq
-              .sortBy(x => x._1.width)
-              .head
+              .take(targetCount)
 
             // logger.debug(metaSelection.groupBy(_.crop).map(x => x._1 -> x._2.length).toSeq.sortBy(x => x._2).mkString("\n"))
             // logger.debug(metaSelection.groupBy(_.crop).map(x => x._1 -> x._2.length).toSeq.sortBy(x => x._1.width).mkString("\n"))
 
 
+            //if (metaSelection.size < targetCount) {
+            //    throw new IllegalArgumentException(s"Insufficent picture count [${metaSelection.size} < ${targetCount}]")
+            //}
             logger.debug("Pruned to " + metaSelection.size)
-            logger.debug("Minimum Crop: " + minCrop._1.width)
 
-            //              .sortBy(dim => (dim.width, dim.height))
-            //              .groupBy(identity)
-            //              .mapValues(_.length)
-            //              .toSeq
-            //              .sortBy(_._2)
-            //              .map(x => s"${x._1} ${x._2}")
-            //              .mkString("\n")
-            // logger.debug(s"${sub}:\n${meta}")
-
-            // cropDirectory(sub)
+            val targetDirectory = s"${sub}_cropped"
+            cropDirectory(sub, targetDirectory, metaSelection)
 
         }
 
 
     }
 
-    def cropDirectory(path: String, dimensions: Map[String, Dimensions]) = {
+    def cropDirectory(sourcePath: String, targetPath: String, metadata: Seq[ImageMetadata]) = {
+
+        // Minimum Pixel Edge Width
+        val minCrop = metadata
+          .groupBy(_.crop)
+          .map(x => x._1 -> x._2.length)
+          .toSeq
+          .sortBy(x => x._1.width)
+          .head
+
 
         val filter: ExtensionFilter = new ExtensionFilter("jpg")
         filter.setRecursion(false);
         val loader: FilenameLoader = new FilenameLoader(filter)
-        val images: java.util.List[String] = loader.loadFilenames(path)
+        val images: java.util.List[String] = loader.loadFilenames(sourcePath)
 
         //        val cropped
         //        val minimumSize =
@@ -121,15 +119,16 @@ object Launcher {
 
         val result = files.par.map { sub =>
 
+            val threadLogger = Logger(s"colla - ${sub}")
+
             val potentialDateTimePropertyNames = Seq("Properties:exif:DateTime", "Properties:exif:Datetime", "Properties:exif:datetime", "Properties:exif:DateTimeOriginal", "Properties:exif:DateTimeDigitized", "Properties:date:create")
-
             val formatter = new java.text.SimpleDateFormat("yyyy:MM:dd HH:mm:ss") // Not threadsafe
-
             val directory = File(sub)
 
             val images = directory.listRecursively.filter(_.extension().get == ".jpg").toSeq
-            logger.debug(s"${sub} with ${images.length} .jpg's")
+            threadLogger.debug(s"${sub} with ${images.length} .jpg's")
             val total = images.length.toFloat
+
             val pkl = images.zipWithIndex.map { imageAndIndex =>
 
                 val index = imageAndIndex._2
@@ -146,7 +145,7 @@ object Launcher {
 
                 val dateTime = dateTimeString match {
                     case None => {
-                        logger.debug(s"Couldn't find datetime for file ${path}")
+                        threadLogger.debug(s"Couldn't find datetime for file ${path}")
                         None
                     }
                     case Some(x) => Some(formatter.parse(x))
@@ -160,25 +159,26 @@ object Launcher {
                     case _ => (total - 1 - index) * (dt / (index + 1))
                 }
 
-                logger.debug(s"$dimensions | ${sub} ${index / total.toFloat} | ${dateTime} | ${dt} | ${remaining}s remaining")
+                threadLogger.debug(s"$dimensions | ${sub} ${index / total.toFloat} | ${dateTime} | ${dt} | ${remaining}s remaining")
 
-                metadata
+                path -> metadata
 
-            }.pickle
+            }.toMap.values.pickle
 
             val outFile = File(s"${sub}.v2.pkl").write(pkl.value)
 
             val unpickledSeq = pkl.unpickle[Seq[ImageMetadata]]
             val sortedEquivalence = unpickledSeq.map(_.path).sorted == images.map(x => x.path.toString).sorted
 
-            logger.debug(s"${directory} -- dictsize: ${unpickledSeq.size} total: ${total}")
+            threadLogger.debug(s"${directory} -- dictsize: ${unpickledSeq.size} total: ${total}")
             if (sortedEquivalence) {
-                logger.debug("Sorted Equivalent")
+                threadLogger.debug("Sorted Equivalent")
             } else {
                 val dictKeys = unpickledSeq.map(_.path).sorted.mkString(",")
                 val dirContents = images.map(x => x.path.toString).toSeq.sorted
-                logger.debug(s"Dict: ${dictKeys}\nDir :${dirContents} ")
+                threadLogger.debug(s"Dict: ${dictKeys}\nDir :${dirContents} ")
             }
+            None
 
         }.toList
 
