bdef730720e8b85b1bed6047c3b21b6371f81faf
kevcmk <kevcmk@gmail.com>
2017-02-28 19:09:35 -0800

    Relayout for single source and much more configurability

--- build.sbt
+++ build.sbt
@@ -4,12 +4,13 @@ version := "1.0"
 
 scalaVersion := "2.11.1"
 
+
 libraryDependencies ++= Seq(
     "org.im4java" % "im4java" % "1.4.0",
     "com.typesafe" % "config" % "1.3.1",
     "com.github.pathikrit" %% "better-files" % "2.17.1",
     "org.scala-lang.modules" %% "scala-pickling" % "0.10.1",
-    "org.apache.commons" % "commons-imaging" % "1.0",
+    "com.drewnoakes" % "metadata-extractor" % "2.10.1",
     "ch.qos.logback" % "logback-classic" % "1.1.7",
     "com.typesafe.scala-logging" %% "scala-logging" % "3.5.0"
 )
--- src/main/scala/io/kevinkatz/cenaculo/Lattice.scala
+++ src/main/scala/io/kevinkatz/cenaculo/Lattice.scala
@@ -1,7 +1,11 @@
+package io.kevinkatz.cenaculo
 
 import java.util.Date
 
 import better.files.File
+import com.drew.imaging.ImageMetadataReader
+import com.drew.metadata.Metadata
+import com.drew.metadata.exif.ExifSubIFDDirectory
 import com.typesafe.config.ConfigFactory
 import com.typesafe.scalalogging.Logger
 import org.im4java.core.{ConvertCmd, IMOperation, Info, MontageCmd}
@@ -14,7 +18,7 @@ import scala.pickling.{functions, json}
 import scala.util.Random
 
 
-object Launcher {
+object Lattice {
 
     val logger = Logger("colla")
 
@@ -22,62 +26,55 @@ object Launcher {
 
         logger.debug("Starting...")
 
-
         ProcessStarter.setGlobalSearchPath(ConfigFactory.load().getString("io.kevinkatz.cenaculo.imagemagick_bin"))
 
-        val dirs = ConfigFactory.load().getStringList("io.kevinkatz.cenaculo.source_directories").asScala
+        val sourceDirectory = File(ConfigFactory.load().getString("io.kevinkatz.cenaculo.source_directory"))
+        val destinationDirectory = File(ConfigFactory.load().getString("io.kevinkatz.cenaculo.destination.directory"))
+        val destinationPrefix = ConfigFactory.load().getString("io.kevinkatz.cenaculo.destination.prefix")
+
+        // Select the largest squares
+        //            val metaSelection = metaAll.toSeq
+        //              .sortBy { x => x.crop.height }
+        //              .reverse
+        //              .take(targetCount)
+
+        // Select a random sample
+
+//        Random.setSeed(42)
+//        val metaSelection = metadata
+//          .filter(_.dateTime.isDefined) // Filter all that had invalid datetimes
+//          .filter(!_.path.contains("crop"))
+//          // .filter(_.crop.width > 1280) // Filter all that are > 1280
+//          .map(x => (Random.nextFloat(), x))
+//          .sortBy(_._1)
+//          .map(_._2)
+//          .take(targetCount)
+
+        // logger.debug(metaSelection.groupBy(_.crop).map(x => x._1 -> x._2.length).toSeq.sortBy(x => x._2).mkString("\n"))
+        // logger.debug(metaSelection.groupBy(_.crop).map(x => x._1 -> x._2.length).toSeq.sortBy(x => x._1.width).mkString("\n"))
 
 
-        // pickleMetadata(dirs)
+        //if (metaSelection.size < targetCount) {
+        //    throw new IllegalArgumentException(s"Insufficent picture count [${metaSelection.size} < ${targetCount}]")
+        //}
+        logger.debug("Pruned to " + metaSelection.size)
 
-
-        dirs.map { sub =>
-
-            logger.debug(s"\n\nSub ${sub}")
-            val metadata = depickle(s"${sub}.v2.pkl")
-
-            logger.debug("Total Size " + metadata.size)
-
-            // val targetCount = 1350
-            val targetCount = 1386
-
-            // Select the largest squares
-            //            val metaSelection = metaAll.toSeq
-            //              .sortBy { x => x.crop.height }
-            //              .reverse
-            //              .take(targetCount)
-
-            // Select a random sample
-
-            Random.setSeed(42)
-            val metaSelection = metadata
-              .filter(_.dateTime.isDefined) // Filter all that had invalid datetimes
-              .filter(!_.path.contains("crop"))
-              // .filter(_.crop.width > 1280) // Filter all that are > 1280
-              .map(x => (Random.nextFloat(), x))
-              .sortBy(_._1)
-              .map(_._2)
-              .take(targetCount)
-
-            // logger.debug(metaSelection.groupBy(_.crop).map(x => x._1 -> x._2.length).toSeq.sortBy(x => x._2).mkString("\n"))
-            // logger.debug(metaSelection.groupBy(_.crop).map(x => x._1 -> x._2.length).toSeq.sortBy(x => x._1.width).mkString("\n"))
-
-
-            //if (metaSelection.size < targetCount) {
-            //    throw new IllegalArgumentException(s"Insufficent picture count [${metaSelection.size} < ${targetCount}]")
-            //}
-            logger.debug("Pruned to " + metaSelection.size)
-
-            val targetDirectory = s"${sub}_cropped"
-            cropAndResizeDirectory(sub, metaSelection)
-            gridDirectory(sub, metaSelection)
-
-        }
+        val resized: File = resizeDirectory(sub, ConfigFactory.load().getInt("io.kevinkatz.cenaculo.square_size"))
+        gridDirectory(
+            source=resized,
+            destinationDirectory=destinationDirectory,
+            aspectHorizontal = ConfigFactory.load().getInt("io.kevinkatz.cenaculo.aspect.horizontal"),
+            aspectVertical = ConfigFactory.load().getInt("io.kevinkatz.cenaculo.aspect.vertical"))
 
 
     }
 
-    def gridDirectory(sourcePath: String, metadata: Seq[ImageMetadata]) = {
+    def gridDirectory(source: File, destinationDirectory: File, aspectHorizontal: Int, aspectVertical: Int) = {
+        if (!source.isDirectory) {
+            throw new IllegalArgumentException("Source must be a directory filled with JPEGs")
+        } else if (!destinationDirectory.exists || !destinationDirectory.isDirectory || !destinationDirectory.isWriteable) {
+            throw new IllegalArgumentException("Destination must be a writable directory")
+        }
         val cmd = new MontageCmd()
         val croppedAndResizedPaths = metadata.sortBy(_.dateTime).map(x => x.path.replace(".jpg", ".crop.256.jpg"))
         val op = new IMOperation()
@@ -92,38 +89,37 @@ object Launcher {
         cmd.run(op)
     }
 
-    def cropAndResizeDirectory(sourcePath: String, metadata: Seq[ImageMetadata]) = {
+    /* Given a path, return a temporary directory with all enclosed jpegs resized to square_size */
+    def resizeDirectory(source: File, square_size: Int, resizedDirectory : Option[File]): File = {
 
-        // Minimum Pixel Edge Width
-        val minCrop = metadata
-          .groupBy(_.crop)
-          .map(x => x._1 -> x._2.length)
-          .toSeq
-          .sortBy(x => x._1.width)
-          .head
-          ._1
-          .width
-
-        logger.debug(s"Cropping to square ${minCrop}")
-//        val filter: ExtensionFilter = new ExtensionFilter("jpg")
-//        filter.setRecursion(false);
-//        val loader: FilenameLoader = new FilenameLoader(filter)
-//        val images: java.util.List[String] = loader.loadFilenames(sourcePath)
+        val outDirectory = resizedDirectory match {
+            case None => File.newTemporaryDirectory()
+            case Some(x) => {
+                if (!x.exists) {
+                    x.createDirectory()
+                } else if (x.isWriteable) {
+                    x
+                } else {
+                    throw new IllegalArgumentException("Resized Directory must be writable")
+                }
+            }
+        }
 
         val cmd = new ConvertCmd();
 
-        metadata.foreach { m =>
+        source.foreach { m =>
 
             val outPath = m.path.replace(".jpg", ".crop.256.jpg")
 
             if (!File(outPath).exists) {
-                    val op = new IMOperation()
-                    op.addImage(m.path) // Input placeholder
-                    op.crop(m.crop.width, m.crop.height, m.crop.x, m.crop.y)
-                    op.resize(256, 256)
-                    op.addImage(outPath)
-                    logger.debug(s"Running ${op.toString}")
-                    cmd.run(op)
+                val crop = crop_square(m.dimensions)
+                val op = new IMOperation()
+                op.addImage(m.path) // Input placeholder
+                op.crop(crop.width, crop.height, crop.x, crop.y)
+                op.resize(square_size, square_size)
+                op.addImage(outPath)
+                logger.debug(s"Running ${op.toString}")
+                cmd.run(op)
             } else {
                 logger.debug(s"Skipping ${outPath} (exists)")
             }
@@ -141,17 +137,15 @@ object Launcher {
 
         val start = java.lang.System.currentTimeMillis()
 
-
-
         val result = files.par.map { sub =>
 
             val threadLogger = Logger(s"colla - ${sub}")
 
-            val potentialDateTimePropertyNames = Seq("Properties:exif:DateTime", "Properties:exif:Datetime", "Properties:exif:datetime", "Properties:exif:DateTimeOriginal", "Properties:exif:DateTimeDigitized", "Properties:date:create")
-            val formatter = new java.text.SimpleDateFormat("yyyy:MM:dd HH:mm:ss") // Not threadsafe
+            // val potentialDateTimePropertyNames = Seq("Properties:exif:DateTime", "Properties:exif:Datetime", "Properties:exif:datetime", "Properties:exif:DateTimeOriginal", "Properties:exif:DateTimeDigitized", "Properties:date:create")
+
             val directory = File(sub)
 
-            val images = directory.listRecursively.filter(_.extension().get == ".jpg").toSeq
+            val images = directory.listRecursively.filter(x => x.extension().get.toLowerCase == ".jpg" || x.extension().get.toLowerCase == ".jpeg").toSeq
             threadLogger.debug(s"${sub} with ${images.length} .jpg's")
             val total = images.length.toFloat
 
@@ -160,24 +154,7 @@ object Launcher {
                 val index = imageAndIndex._2
                 val path = imageAndIndex._1.path.toString
 
-                val info = new Info(path.toString)
-
-                val dimensions = new Dimensions(info.getImageWidth, info.getImageHeight)
-                val crop = crop_square(dimensions)
-
-                val dateTimeString = potentialDateTimePropertyNames
-                  .map(x => info.getProperty(x))
-                  .filter(_ != null).headOption
-
-                val dateTime = dateTimeString match {
-                    case None => {
-                        threadLogger.debug(s"Couldn't find datetime for file ${path}")
-                        None
-                    }
-                    case Some(x) => Some(formatter.parse(x))
-                }
-
-                val metadata = ImageMetadata(path, dimensions, crop, dateTime)
+                val metadata = getImageMetadata(path.toString)
 
                 val dt = (java.lang.System.currentTimeMillis() - start) / 1000.0
                 val remaining = index match {
@@ -185,7 +162,7 @@ object Launcher {
                     case _ => (total - 1 - index) * (dt / (index + 1))
                 }
 
-                threadLogger.debug(s"$dimensions | ${sub} ${index / total.toFloat} | ${dateTime} | ${dt} | ${remaining}s remaining")
+                threadLogger.debug(s"${metadata.dimensions} | ${sub} ${index / total.toFloat} | ${metadata.dateTime} | ${dt} | ${remaining}s remaining")
 
                 path -> metadata
 
@@ -233,6 +210,48 @@ object Launcher {
         override def toString = s"$width $height $x $y"
     }
 
-    case class ImageMetadata(path: String, dimensions: Dimensions, crop: CropBox, dateTime: Option[Date])
+    case class ImageMetadata(path: String, dimensions: Dimensions, dateTime: Option[Date])
+
+    def getImageMetadata(path: String): Option[ImageMetadata] = {
+
+        val metadata: Metadata = ImageMetadataReader.readMetadata(File(path).toJava);
+        val directory: ExifSubIFDDirectory = metadata.getFirstDirectoryOfType(classOf[ExifSubIFDDirectory]);
+
+        val TAG_DATETIME_ORIGINAL = 0x9003
+        val TAG_DATETIME = 0x0132
+        val TAG_DATETIME_DIGITIZED: Int = 0x9004
+        val dateTime: Option[Date] = Seq(
+            directory.getDateOriginal(),
+            directory.getDate(TAG_DATETIME_ORIGINAL),
+            directory.getDate(TAG_DATETIME),
+            directory.getDate(TAG_DATETIME_DIGITIZED)
+        ).filter(_ != null).headOption
+
+        val TAG_IMAGE_WIDTH: Int = 0x0100
+        val TAG_IMAGE_HEIGHT: Int = 0x0101
+        val dimensions: Dimensions = (Option(directory.getInt(TAG_IMAGE_WIDTH)), Option(directory.getInt(TAG_IMAGE_HEIGHT))) match {
+            case (Some(width), Some(height)) => Dimensions(width, height)
+            case _ => {
+                // Fallback method which actually loads image
+                logger.warn(s"EXIF Dimensions Missing for image $path, retrieving manually")
+                val info = new Info(path.toString)
+                val dimensions = new Dimensions(info.getImageWidth, info.getImageHeight)
+            }
+        }
+
+        if (dateTime.isEmpty) {
+            logger.warn(s"EXIF Date Missing for image $path ")
+            None
+        } else {
+            Some(ImageMetadata(path, dimensions, dateTime))
+        }
+
+
+        //val dateTimeString = potentialDateTimePropertyNames
+        //  .map(x => info.getProperty(x))
+        //  .filter(_ != null).headOption
+
+
+    }
 
 }
